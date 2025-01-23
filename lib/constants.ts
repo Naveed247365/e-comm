d22adee9bb9c119aac6c210005c9f19504866c22
Naveed247365 <qureshinaveed21@hotmail.com>
2025-01-23 03:10:58 -0800

    03-create website layout

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXR_PUBLIC_APP_NAME || "ecomm";
+export const APP_SLOGAN= process.env.NEXT_PUBLIC_APP_SLOGAN || "spend less, enjoy more.";
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "An ecomm clone with Next.js and MongoDB";
---
+++ lib/data.ts
@@ -0,0 +1,38 @@
+const data = {
+    headerMenu: [
+        {
+            name: "Today's Deals",
+            href: "/search?tag=todays-deals",
+        },
+        {
+            name: "New Arrivals",
+            href: "/search?tag=new-arrivals",
+        },
+        {
+            name: "Featured Products",
+            href: "/search?tag=featured",
+        },
+        {
+            name: "Best Sellers",
+            href: "/search?tag=best-sellers",
+        },
+        {
+            name: "Browsing History",
+            href: "/#browsing-history",
+        },
+        {
+            name: "Customer Service",
+            href: "/page/customer-service",
+        },
+        {
+            name: "About Us",
+            href: "/page/about-us",
+        },
+        {
+            name: "help",
+            href: "/page/help",
+        },
+    ],
+}
+
+export default data;
