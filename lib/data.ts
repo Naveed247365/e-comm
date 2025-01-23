8cc18b4ebe9f8d7ef6266bd1213a771289fe7e8c
Naveed247365 <qureshinaveed21@hotmail.com>
2025-01-23 10:55:42 -0800

    04-create-home-carousel

--- lib/data.ts
+++ lib/data.ts
@@ -1,38 +1,64 @@
+import { url } from 'inspector';
+import { title } from 'process';
+
 const data = {
-    headerMenu: [
-        {
-            name: "Today's Deals",
-            href: "/search?tag=todays-deals",
-        },
-        {
-            name: "New Arrivals",
-            href: "/search?tag=new-arrivals",
-        },
-        {
-            name: "Featured Products",
-            href: "/search?tag=featured",
-        },
-        {
-            name: "Best Sellers",
-            href: "/search?tag=best-sellers",
-        },
-        {
-            name: "Browsing History",
-            href: "/#browsing-history",
-        },
-        {
-            name: "Customer Service",
-            href: "/page/customer-service",
-        },
-        {
-            name: "About Us",
-            href: "/page/about-us",
-        },
-        {
-            name: "help",
-            href: "/page/help",
-        },
-    ],
-}
+  headerMenu: [
+    {
+      name: "Today's Deals",
+      href: '/search?tag=todays-deals',
+    },
+    {
+      name: 'New Arrivals',
+      href: '/search?tag=new-arrivals',
+    },
+    {
+      name: 'Featured Products',
+      href: '/search?tag=featured',
+    },
+    {
+      name: 'Best Sellers',
+      href: '/search?tag=best-sellers',
+    },
+    {
+      name: 'Browsing History',
+      href: '/#browsing-history',
+    },
+    {
+      name: 'Customer Service',
+      href: '/page/customer-service',
+    },
+    {
+      name: 'About Us',
+      href: '/page/about-us',
+    },
+    {
+      name: 'help',
+      href: '/page/help',
+    },
+  ],
+  carousels: [
+    {
+      title: 'Most Popular Shoes For Sale',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner3.jpg',
+      url: '/search?category=Shoes',
+      isPublished: true,
+    },
+    {
+      title: 'Best Sellers in T-Shirts',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner1.jpg',
+      url: '/search?category=T-Shirts',
+      isPublished: true,
+    },
+    {
+      title: 'Best Deals on Wrist Watches',
+      buttonCaption: 'See More',
+      image: '/images/banner2.jpg',
+      url: '/search?category=Wrist watches',
+      isPublished: true,
+    },
+  ],
+};
 
 export default data;
